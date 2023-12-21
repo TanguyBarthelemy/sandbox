92e576ae01ad1ceaa39604ac20cb5c33bf0bd5d2
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-12-21 16:08:11 +0100

    new testsÃ

--- R/simple_program.R
+++ R/simple_program.R
@@ -1,5 +1,61 @@
-f <- function(a) {
-    sum(k)
-}
-print(tryCatch(f(a = "AAA"), error = function(e) e))
-print(tryCatch(f(a = 1), error = function(e) e))
+
+# Sys.setenv(LANGUAGE="de") 
+# Sys.setenv(lang = "de") 
+# 
+# Sys.setlocale("LC_MESSAGES", "en_US.utf8") 
+# 
+# "LC_COLLATE=French_France.utf8;LC_CTYPE=French_France.utf8;LC_MONETARY=French_France.utf8;LC_NUMERIC=C;LC_TIME=French_France.utf8" 
+# 
+# 
+# Sys.setlocale("LC_TIME", "C") 
+# Sys.setlocale(locale = "ru_RU.utf8") 
+# 
+# Sys.setlocale("LC_TIME", "de_DE.utf8") 
+# Sys.setlocale("LC_TIME", "fr_CA.UTF-8") 
+# 
+# tryCatch({1 + x}, error = function(e) print(e)) 
+# print(format(Sys.Date(), "%Y-%b-%d")) 
+
+
+M_Drucker_birth <- base::as.Date(x = "1942-09-12")
+noel_2023 <- base::as.Date(x = "25/12/2023", format = "%d/%m/%Y")
+
+
+pied_sur_la_lune <- as.POSIXct(x = "1969-07-21 02:56:20", 
+                               format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+
+heure_en_france <- as.POSIXct(x = pied_sur_la_lune, tz = "Europe/Paris")
+heure_los_angeles <- as.POSIXct(x = pied_sur_la_lune, tz = "America/Los_Angeles")
+
+# définition en date et heure locale avec le bon fuseau horaire
+chute_mur_berlin <- as.POSIXct(x = "1989-11-09 18:00", tz = "Europe/Berlin")
+
+# Heure locale
+print(chute_mur_berlin)
+
+# Heure UTC (exemple en islande)
+print(as.POSIXct(chute_mur_berlin, tz = "UTC"))
+
+# définition en date et heure locale avec le bon fuseau horaire
+victoire_fifa_1998 <- as.POSIXct(x = "1998-07-12 21:00", tz = "Europe/Paris")
+
+# Heure locale
+print(victoire_fifa_1998)
+
+# Heure UTC (exemple au Burkina Faso)
+print(as.POSIXct(victoire_fifa_1998, tz = "UTC"))
+
+
+# Paramètres locaux en France
+Sys.setlocale("LC_TIME", "fr_FR.UTF-8")
+format(Sys.time(), format = "%c")
+
+# Paramètres locaux aux USA
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+format(Sys.time(), format = "%c")
+
+# Paramètres locaux aux USA
+Sys.setlocale("LC_TIME", "C")
+format(Sys.time(), format = "%c")
+
+
