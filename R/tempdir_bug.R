bfbaa77c36aee939dbee8f7b00923ec6b918b844
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-01-31 14:05:39 +0100

    add writing and reading test

--- R/tempdir_bug.R
+++ R/tempdir_bug.R
@@ -1,13 +1,28 @@
 
-install.packages(c("rJava", "RJDemetra", "remotes"))
-remotes::install_github("InseeFrLab/rjdworkspace")
+# install.packages(c("rJava", "RJDemetra", "remotes"))
+# remotes::install_github("InseeFrLab/rjdworkspace")
 
 library("RJDemetra")
 library("rjdworkspace")
 
 dir_ws <- tempdir()
+
+print(dir_ws)
+print(normalizePath(dir_ws))
+
+new_path <- file.path(dir_ws, "test.csv")
+
+write.table(iris, file = new_path)
+
+new_iris <- read.table(new_path)
+print(head(new_iris))
+
+
+# rjdworkspace part ------------------------------------------------------------
+
 template_ws <- file.path(system.file("extdata", package = "rjdworkspace"),
                          "WS")
+
 # Moving the WS in a temporary environment
 rjdworkspace:::copy_ws(
     ws_name = "ws_output",
