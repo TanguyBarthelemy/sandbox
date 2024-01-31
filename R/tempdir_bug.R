e88fdf685de2e8f13269fde075063acc8dbb19e1
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-01-31 14:26:44 +0100

    printt

--- R/tempdir_bug.R
+++ R/tempdir_bug.R
@@ -5,21 +5,30 @@
 library("RJDemetra")
 library("rjdworkspace")
 
+printtt <- function(obj) {
+    obj_name <- deparse(substitute(obj))
+    cat(obj_name, ":", obj, "\n")
+    return(invisible(NULL))
+}
+
+
+# temp dir ---------------------------------------------------------------------
+
 dir_ws <- tempdir()
 
-print(dir_ws)
-print(normalizePath(dir_ws))
-print(dir.exists(dir_ws))
+printt(dir_ws)
+printt(normalizePath(dir_ws))
+printt(dir.exists(dir_ws))
 
-print(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
+printt(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
 
 new_path <- file.path(dir_ws, "test.csv")
 write.table(iris, file = new_path)
 
-print(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
+printt(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
 
 new_iris <- read.table(new_path)
-print(head(new_iris))
+printt(head(new_iris))
 
 
 # rjdworkspace part ------------------------------------------------------------
@@ -42,13 +51,13 @@ rjdworkspace:::copy_ws(
 path_ws_from <- file.path(dir_ws, "ws_input.xml")
 path_ws_to <- file.path(dir_ws, "ws_output.xml")
 
-print(path_ws_from)
-print(path_ws_to)
+printt(path_ws_from)
+printt(path_ws_to)
 
-print(normalizePath(path_ws_to))
-print(normalizePath(path_ws_from))
+printt(normalizePath(path_ws_to))
+printt(normalizePath(path_ws_from))
 
-print(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
+printt(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
 
 ws_input <- load_workspace(path_ws_from)
 ws_output <- load_workspace(path_ws_to)
