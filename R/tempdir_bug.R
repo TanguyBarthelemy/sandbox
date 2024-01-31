737329463cab507574e69d2b8794472b8c0cea50
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-01-31 14:21:20 +0100

    add writing and reading test

--- R/tempdir_bug.R
+++ R/tempdir_bug.R
@@ -11,12 +11,12 @@ print(dir_ws)
 print(normalizePath(dir_ws))
 print(dir.exists(dir_ws))
 
-print(list.files(dir_ws, all.files = TRUE))
+print(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
 
 new_path <- file.path(dir_ws, "test.csv")
 write.table(iris, file = new_path)
 
-print(list.files(dir_ws, all.files = TRUE))
+print(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
 
 new_iris <- read.table(new_path)
 print(head(new_iris))
@@ -38,8 +38,17 @@ rjdworkspace:::copy_ws(
     path_ws = template_ws,
     new_path = dir_ws
 )
+
 path_ws_from <- file.path(dir_ws, "ws_input.xml")
 path_ws_to <- file.path(dir_ws, "ws_output.xml")
+
+print(path_ws_from)
+print(path_ws_to)
+
+print(normalizePath(path_ws_to))
+print(normalizePath(path_ws_from))
+
+print(list.files(dir_ws, all.files = TRUE, recursive = TRUE))
 
 ws_input <- load_workspace(path_ws_from)
 ws_output <- load_workspace(path_ws_to)
