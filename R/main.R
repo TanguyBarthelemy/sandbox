eaca67c08577c542e2f45678f2d3b6e8c2b6d284
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-06-05 15:26:23 +0200

    change and test wd

--- R/main.R
+++ R/main.R
@@ -1,3 +1,6 @@
+
+print(getwd())
+
 install.packages("remotes")
 remotes::install_github("TanguyBarthelemy/TBox")
 
@@ -56,8 +59,8 @@ rmd_content <- f("pdf", eval = TRUE, font_size = 16)
 ext <- ".pdf"
 
 
-rmd_file <- "./Rmd/format_code.Rmd"
-out_file <- "./Rmd/format_code.pdf"
+rmd_file <- "../Rmd/format_code.Rmd"
+out_file <- "../Rmd/format_code.pdf"
 
 write(rmd_content, file = rmd_file)
 rmarkdown::render(input = rmd_file, output_file = out_file)
