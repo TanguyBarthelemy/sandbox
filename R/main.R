7fffb645a908153c0734d0bc13294119f00810d1
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-06-05 15:05:23 +0200

    update rmd as in TBox

--- R/main.R
+++ R/main.R
@@ -1,4 +1,63 @@
 install.packages("remotes")
 remotes::install_github("TanguyBarthelemy/TBox")
 
-rmarkdown::render(input = "./Rmd/format_code.Rmd", output_file = "./out.pdf")
+
+f <- function(output = "word",
+              browser = getOption("browser"),
+              eval = FALSE,
+              font_size = 12,
+              code = TRUE) {
+    
+    rmd_header <- paste0(
+        "---\ntitle: \"Format code\"\noutput:\n  ",
+        output,
+        "_document:\n    highlight: arrow\n",
+        ifelse(
+            test = (output == "pdf"),
+            yes = "    latex_engine: xelatex\n",
+            no = "monofont: \"Fira Code\"\n"
+        ),
+        "code-block-bg: true\n",
+        "code-block-border-left: \"#31BAE9\"\n",
+        "---\n"
+    )
+    rmd_pdf <- ifelse(
+        test = (output == "pdf"),
+        yes = paste0(
+            "\n\\fontsize{", font_size, "}{", font_size, "}\n",
+            "\\setmonofont[ExternalLocation=",
+            system.file("extdata", "FiraCode", package = "TBox"),
+            "/]{FiraCode-Regular.ttf}\n"
+        ),
+        no = ""
+    )
+    
+    content <- "plot(AirPassengers)"
+    
+    rmd_body <- paste0(
+        "\n## Running Code\n\n",
+        ifelse(
+            test = code,
+            yes = paste0("```{r, echo = TRUE, eval = ", eval, "}"),
+            no = ""
+        ), "\n",
+        content, "\n",
+        ifelse(code, "```", ""), "\n"
+    )
+    
+    rmd_content <- paste0(rmd_header, rmd_pdf, rmd_body)
+    
+    return(rmd_content)
+}
+
+rmd_content <- f("pdf", eval = TRUE, font_size = 16)
+
+
+ext <- ".pdf"
+
+
+rmd_file <- tempfile(pattern = "template", fileext = ".Rmd")
+out_file <- tempfile(pattern = "output", fileext = ext)
+
+write(rmd_content, file = rmd_file)
+rmarkdown::render(input = rmd_file, output_file = out_file)
